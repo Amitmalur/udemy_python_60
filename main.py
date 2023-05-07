2a7a96dc5285f5890539de0aa6c515207395f05c
Amit Malur <amitmalur@gmail.com>
2023-05-07 19:34:36 +0530

    get_todos with argument

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+def get_todos(filepath):
+    with open(filepath, "r") as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
     if user_action.startswith("add"):
         todo = user_action[4:]
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos("todos.txt")
         todos.append(todo + "\n")
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
     elif user_action.startswith("show"):
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos("todos.txt")
         for index, item in enumerate(todos):
             item = item.strip('\n')
             item = item.capitalize()
@@ -19,22 +23,23 @@ while True:
         try:
             todo_number = int(user_action[5:])
             todo_number = todo_number-1
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos("todos.txt")
             print("Here is todos existing", todos)
 
             new_todo = input("Enter new ToDo item:")
             todos[todo_number] = new_todo + "\n"
             with open('todos.txt', 'w') as file:
                 file.writelines(todos)
+        except IndexError:
+            print("Your command is not Valid")
+            continue
         except ValueError:
             print("Your command is not Valid")
             continue
     elif user_action.startswith("complete"):
         try:
             todo_number = int(user_action[9:])
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos("todos.txt")
             todo_number = todo_number-1
             todo_to_remove = todos[todo_number].strip("\n")
             todos.pop(todo_number)
