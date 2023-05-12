e67fa6e5b82361f9711942507e86cfa450cca38e
Amit Malur <amitmalur@gmail.com>
2023-05-12 23:36:26 +0530

    Added doc string for get_todos and write_todos

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def get_todos(filepath):
+    """Read a text file and return the list of To-Do items"""
     with open(filepath, "r") as file:
         todos_local = file.readlines()
     return todos_local
 
 
 def write_todos(filepath, todos_arg):
+    """Write list of To-Do items into text file"""
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
 
