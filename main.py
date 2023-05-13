3175ed8666f68f9380f93eb9fd428d8fe26f003a
Amit Malur <amitmalur@gmail.com>
2023-05-13 07:37:04 +0530

    Created a new file for functions for import

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-def get_todos(filepath):
-    """Read a text file and return the list of To-Do items"""
-    with open(filepath, "r") as file:
-        todos_local = file.readlines()
-    return todos_local
-
-
-def write_todos(filepath, todos_arg):
-    """Write list of To-Do items into text file"""
-    with open(filepath, "w") as file:
-        file.writelines(todos_arg)
-
+from functions import get_todos, write_todos
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
