ca96818cd1cba08350e1e0ee5090265a8d47c322
Amit Malur <amitmalur@gmail.com>
2023-05-20 13:37:11 +0530

    Functionality to add todo list close the window

--- bonus1.py
+++ bonus1.py
@@ -1,3 +1,20 @@
-user_input = input("Enter the title: ")
-text = len(user_input)
-print("Length of the title:", text)
+import PySimpleGUI as sg
+
+
+label1 = sg.Text("Select files to compress:")
+input1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose")
+
+label2 = sg.Text("Select destination folder:")
+input2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose")
+
+compress_button = sg.Button("Compress")
+
+window = sg.Window("File Compressor", layout=[[label1, input1, choose_button1],
+                                              [label2, input2, choose_button2],
+                                              [compress_button]])
+
+
+window.read()
+window.close()
