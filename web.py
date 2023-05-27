d9f25ca4a2f5b740f916f821daff2e87a04954ea
Amit Malur <amitmalur@gmail.com>
2023-05-27 10:43:55 +0530

    Completed Todo app development

--- web.py
+++ web.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 import functions
 
 
@@ -16,8 +15,13 @@ st.title("My To App")
 st.subheader("This is my to do App")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="Enter a To Do:", placeholder="Add a To Do",
               on_change=add_todo, key="new_todo")
